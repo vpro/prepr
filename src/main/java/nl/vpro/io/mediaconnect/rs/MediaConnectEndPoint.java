46da8a41b526bdbcdf5fecbfc13dcb4970d035d9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-04 13:43:05 +0200

    javadoc.

--- src/main/java/nl/vpro/io/mediaconnect/rs/MediaConnectEndPoint.java
+++ src/main/java/nl/vpro/io/mediaconnect/rs/MediaConnectEndPoint.java
@@ -9,6 +9,33 @@ import javax.ws.rs.NameBinding;
 
 /**
  *
+ * This annotation can be used on your JAXRS implementation of a webhook callback. Because {@link SignatureValidatorInterceptor} has it too, the signatures will be validated implicitly than.
+ *
+ * E.g.:
+ *<pre>{@code
+@literal @Path("/mediaconnect")
+ public class MediaConnectWebhookEndpoint {
+   @literal @POST
+   @literal @Path("/{channel}")
+   @literal @MediaConnectEndPoint
+    public String post(
+       @literal @PathParam("channel") Channel channel,
+        JsonNode payload,
+       @literal @Context HttpServletRequest request
+    ) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        MCObjectMapper.INSTANCE.writeValue(output, payload);
+
+        jms.convertAndSend(jmsQueue, output.toByteArray(), new MessagePostProcessor() {
+        return "Accepted to queue " + jmsQueue;
+    }
+})
+}
+}</pre>
+ *
+ *
+ *
  * @author Michiel Meeuwissen
  * @since 0.1
  */
