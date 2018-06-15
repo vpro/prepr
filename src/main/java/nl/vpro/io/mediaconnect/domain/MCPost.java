dc5f8c15e3c13b27efde314a9cf7f29db878c7b1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-15 16:50:50 +0200

    toString.

--- src/main/java/nl/vpro/io/mediaconnect/domain/MCPost.java
+++ src/main/java/nl/vpro/io/mediaconnect/domain/MCPost.java
@@ -2,6 +2,7 @@ package nl.vpro.io.mediaconnect.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @JsonTypeName("Post")
 public class MCPost extends MCContent {
 
