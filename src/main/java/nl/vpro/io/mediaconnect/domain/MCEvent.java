c217118cbe9e4222cab8876da5484590db8316a8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-09-04 14:31:54 +0200

    MSE-4147

--- src/main/java/nl/vpro/io/mediaconnect/domain/MCEvent.java
+++ src/main/java/nl/vpro/io/mediaconnect/domain/MCEvent.java
@@ -3,13 +3,18 @@ package nl.vpro.io.mediaconnect.domain;
 import lombok.Data;
 
 import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.util.StdConverter;
 
 /**
  * @author Michiel Meeuwissen
  * @since 0.1
  */
 @Data
+@JsonDeserialize(converter= MCEvent.MCEventDeserializer.class)
 public class MCEvent {
 
 
@@ -34,5 +39,24 @@ public class MCEvent {
     MCUsers users;
 
     List<MCTimeline> timelines;
+
+
+    /**
+     * We want the timelines to be in a logical order. That is, the first one first.
+     * We get them in a kind of random order back from the API.
+     *
+     * We use the _first_ timeline to generate a ID, so it should always be the same one.
+     */
+
+    public static class MCEventDeserializer extends StdConverter<MCEvent, MCEvent> {
+
+        @Override
+        public MCEvent convert(MCEvent mcEvent) {
+            if (mcEvent != null && mcEvent.timelines != null) {
+                mcEvent.timelines.sort(Comparator.comparing(MCTimeline::getFrom));
+            }
+            return mcEvent;
+        }
+    }
 
 }
