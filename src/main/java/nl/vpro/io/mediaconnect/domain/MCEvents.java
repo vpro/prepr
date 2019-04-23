8e2e2b596d73a781dfee9f2a795d1c0595e6731c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-04-23 17:22:57 +0200

    More event handling.

--- src/main/java/nl/vpro/io/mediaconnect/domain/MCEvents.java
+++ src/main/java/nl/vpro/io/mediaconnect/domain/MCEvents.java
@@ -6,18 +6,32 @@ package nl.vpro.io.mediaconnect.domain;
  */
 public class MCEvents {
 
+    private static final String CREATED  =  ".created";
+    private static final String CHANGED =  ".changed";
+    private static final String DELETED =  ".deleted";
+    private static final String PUBLISHED=  ".published";
+    private static final String UNPUBLISHED=  ".unpublished";
+
+
     static final String SHOWSCHEDULE = "showschedule";
 
-    public static final String SHOWSCHEDULE_CREATED = SHOWSCHEDULE + ".created";
+    public static final String SHOWSCHEDULE_CREATED = SHOWSCHEDULE + CREATED;
 
-    public static final String SHOWSCHEDULE_CHANGED = SHOWSCHEDULE + ".changed";
+    public static final String SHOWSCHEDULE_CHANGED = SHOWSCHEDULE + CHANGED;
 
-    public static final String SHOWSCHEDULE_DELETED = SHOWSCHEDULE + ".deleted";
+    public static final String SHOWSCHEDULE_DELETED = SHOWSCHEDULE + DELETED;
 
 
     static final String PUBLICATION = "publication";
 
-    public static final String PUBLICTICATION_PUBLISHED = PUBLICATION + ".published";
-    public static final String PUBLICTICATION_CHANGED = PUBLICATION + ".changed";
+    public static final String PUBLICATION_CREATED = PUBLICATION + CREATED;
+
+    public static final String PUBLICATION_PUBLISHED = PUBLICATION + PUBLISHED;
+
+    public static final String PUBLICATION_UNPUBLISHED = PUBLICATION + UNPUBLISHED;
+
+    public static final String PUBLICATION_CHANGED = PUBLICATION + CHANGED;
+
+    public static final String PUBLICATION_DELETED = PUBLICATION + DELETED;
 
 }
