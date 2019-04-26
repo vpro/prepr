89bb0e67c7a3ede96104e5fff6f1888b38d2a315
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-04-26 15:40:23 +0200

    Made 'isSegment' a bit smarter.

--- src/main/java/nl/vpro/io/mediaconnect/domain/MCAbstractMedia.java
+++ src/main/java/nl/vpro/io/mediaconnect/domain/MCAbstractMedia.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.DurationSerializer;
+import com.google.common.collect.Range;
 
 /**
  * @author Michiel Meeuwissen
@@ -24,13 +25,25 @@ public abstract class MCAbstractMedia extends MCAsset {
     Instant ended_on;
 
 
-    public boolean isSegment() {
+    public boolean isSegment(Range<Instant> schedule) {
         // TODO: Segmenten worden eigenlijk helemaal niet ondersteund in Prepr.
         // Je kunt in de user interface allerlei filmpjes uploaden, het is in het geheel niet gezegd dat dat correspondeert met de uitzending.
         // Misschien moeten we hier als extra check inbouwen dat we zeker weten dat dit oorspronkelijk een radiobox object was?
         // Andere optie is om hier te retourneren: false. Het wordt niet ondersteund.
 
-        return getStarted_on() != null && getDuration() != null;
+        if (getStarted_on() == null) {
+            return false;
+        }
+        Range<Instant> range;
+        if (getDuration() != null) {
+            range = Range.closedOpen(getStarted_on(), getStarted_on().plus(getDuration()));
+        } else if (getEnded_on() != null) {
+            range = Range.closedOpen(getStarted_on(), getEnded_on());
+        } else {
+            return false;
+        }
+
+        return schedule.encloses(range);
 
     }
 
