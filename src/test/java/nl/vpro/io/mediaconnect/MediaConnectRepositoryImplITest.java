2be9c68241c1af3684368867e35c5f95f7c90328
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-04-26 14:37:20 +0200

    Delete image feature for prepr.

--- src/test/java/nl/vpro/io/mediaconnect/MediaConnectRepositoryImplITest.java
+++ src/test/java/nl/vpro/io/mediaconnect/MediaConnectRepositoryImplITest.java
@@ -66,7 +66,7 @@ public class MediaConnectRepositoryImplITest {
 
     @Test
     public void getSchedule() {
-        getSchedule(funx, LocalDate.of(2019, 1, 2));
+        getSchedule(fnxa, LocalDate.of(2019, 4, 22));
     }
 
 
