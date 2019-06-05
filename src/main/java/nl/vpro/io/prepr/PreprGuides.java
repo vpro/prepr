74bda24d9f144d3a89a35bfff320b147aadd2a8e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-06-05 11:16:58 +0200

    MSE-4502

--- src/main/java/nl/vpro/io/prepr/PreprGuides.java
+++ src/main/java/nl/vpro/io/prepr/PreprGuides.java
@@ -3,17 +3,12 @@ package nl.vpro.io.prepr;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.annotation.Nonnull;
 
-import org.slf4j.LoggerFactory;
-import com.google.common.collect.Range;
-
-import nl.vpro.io.prepr.domain.MCGuide;
-import nl.vpro.io.prepr.domain.MCItems;
-import nl.vpro.io.prepr.domain.MCSchedule;
+import nl.vpro.io.prepr.domain.*;
 
 /**
  * @author Michiel Meeuwissen
@@ -24,26 +19,21 @@ public interface PreprGuides {
 
     MCSchedule getSchedule(LocalDate from, LocalDate until, boolean exceptions, UUID showId);
 
-    default MCSchedule getSchedule(@Nonnull  LocalDateTime from, @Nonnull  LocalDateTime until) {
-        Range<LocalDateTime> range = Range.closedOpen(from, until);
-        LocalDate fromDate = from.toLocalDate();
-        LocalDate untilDate = until.toLocalDate();
+    /**
+     * Performs {@link #getSchedule(LocalDateTime, LocalDateTime)}, and wraps the result into a list of {@link MCEventWithDay}
+     */
+
+    default List<MCEventWithDay> getSchedule(@Nonnull  LocalDateTime from, @Nonnull  LocalDateTime until) {
+
+        // broadcasts may span 0 o'clock, so we need the day before and after too, to make sure that we get only _complete_ broadcasts
+        LocalDate fromDate = from.toLocalDate().minusDays(1);
+        LocalDate untilDate = until.toLocalDate().plusDays(1);
         if (Duration.between(untilDate.atStartOfDay(), until).toMinutes() == 0) {
+            // Well if the until day is exactly at the start of day, so actually belonging to the next day, we don't need any data for it.
             untilDate = untilDate.minusDays(1);
         }
-        MCSchedule result = getSchedule(fromDate, untilDate, true, null);
-        result.forEach((e) -> {
-            e.setValue(new ArrayList<>(e.getValue()));
-            e.getValue().removeIf(event -> {
-                Range<LocalDateTime> erange = event.getRange(e.getKey());
-                boolean startInRange = range.contains(erange.lowerEndpoint());
-                if (! startInRange) {
-                    LoggerFactory.getLogger(PreprGuides.class).info("{} not in {}: Removing {})", erange.lowerEndpoint(), range, event);
-                }
-                return ! startInRange;
-            });
-        });
-        return result;
+        MCSchedule unfilteredResult = getSchedule(fromDate, untilDate, true, null);
+        return MCEventWithDay.fromSchedule(unfilteredResult, from, until);
     }
 
     default MCSchedule getSchedule(LocalDate from, LocalDate until) {
@@ -55,7 +45,6 @@ public interface PreprGuides {
     }
 
      MCItems<MCGuide> getGuides(String q);
-
 
 
 }
---
+++ src/main/java/nl/vpro/io/prepr/domain/MCEventWithDay.java
@@ -0,0 +1,106 @@
+package nl.vpro.io.prepr.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
+import com.google.common.collect.Range;
+
+/**
+ * The guides call returns data in format which is often unsuitable for processing. It will group by day, but broadcasts may span days.
+ * Also, the contained event objects are not self contained, and need the day to calculate the actual time.
+ *
+ * This class wraps the {@link MCEvent} with {@link LocalDate} object to get a complete small bundle of information which can be used as an entry in lists which represent a schedule.
+ *
+ * The utility {@link #asRange()} returns the actual range the event is representing.
+ *
+ * The {@link #append(MCEventWithDay)} utility targets to be able to 'glue' events together.
+ *
+ * @author Michiel Meeuwissen
+ * @since 0.9
+ */
+@Getter
+@ToString
+@Slf4j
+public class MCEventWithDay {
+    private final LocalDate day;
+    private final MCEvent mcEvent;
+
+    MCEventWithDay next = null;
+
+
+    MCEventWithDay(LocalDate day, MCEvent mcEvent) {
+        this.day = day;
+        this.mcEvent = mcEvent;
+    }
+
+    public Range<LocalDateTime> asRange() {
+
+        Range<LocalDateTime> range = mcEvent.getRange(day);
+        if (next != null) {
+            return range.span(next.asRange());
+        } else {
+            return range;
+        }
+    }
+
+    public void append(MCEventWithDay next) {
+        if (this.next == null) {
+            this.next = next;
+        } else {
+            this.next.append(next);
+        }
+    }
+
+    public String showId() {
+        return mcEvent.getTimelines().stream().map(MCContent::getReference_id).findFirst().orElse(null);
+    }
+
+
+    public static List<MCEventWithDay> fromSchedule(@Nonnull MCSchedule unfilteredResult) {
+        List<MCEventWithDay> result = new ArrayList<>();
+
+        unfilteredResult.forEach((e) -> {
+            for (MCEvent mcEvent : e.getValue()) {
+                MCEventWithDay withDay = new MCEventWithDay(e.getKey(), mcEvent);
+                String showId = withDay.showId();
+                if (result.size() > 0) {
+                    MCEventWithDay previous = result.get(result.size() - 1);
+                    String previousShowId = previous.showId();
+                    if (Objects.equals(showId, previousShowId)) {
+                        log.debug("Appending {} to {}", withDay, previous);
+                        previous.append(withDay);
+                        continue;
+                    }
+                }
+                result.add(withDay);
+            }
+        });
+        return result;
+    }
+
+    public static List<MCEventWithDay> fromSchedule(@Nonnull MCSchedule unfilteredResult, @Nonnull LocalDateTime from, @Nonnull LocalDateTime until) {
+        Range<LocalDateTime> range = Range.closedOpen(from, until);
+        List<MCEventWithDay> result = fromSchedule(unfilteredResult);
+
+        result.removeIf(event -> {
+            Range<LocalDateTime> erange = event.asRange();
+            boolean startInRange = range.contains(erange.lowerEndpoint());
+            if (!startInRange) {
+                log.debug("{} not in {}: Removing {})", erange.lowerEndpoint(), range, event);
+            }
+            return !startInRange;
+        });
+        return result;
+
+    }
+
+}
