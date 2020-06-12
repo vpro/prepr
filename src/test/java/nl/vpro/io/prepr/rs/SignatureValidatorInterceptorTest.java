47e7a49a81641cb53773bbeb8f42274ef4066ba4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-12 15:47:04 +0200

    rollback

--- src/test/java/nl/vpro/io/prepr/rs/SignatureValidatorInterceptorTest.java
+++ src/test/java/nl/vpro/io/prepr/rs/SignatureValidatorInterceptorTest.java
@@ -16,14 +16,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SignatureValidatorInterceptorTest {
 
 
-    byte[] example = "{\"id\":\"6079a3de-6387-4891-96dc-626c659a4269\",\"created_on\":1591959460,\"dc\":\"api.eu1.prepr.io\",\"event\":\"asset.created\",\"payload\":{\"id\":\"d7fdb12e-d244-4576-a10d-ffc4c60dd5f3\",\"created_on\":\"2020-06-11T22:51:31+00:00\",\"changed_on\":\"2020-06-11T22:51:31+00:00\",\"label\":\"Cover\",\"name\":\"1591915891\",\"body\":null,\"author\":null,\"reference_id\":null}}".getBytes();
-    String signature = "93c76a323a95f1b4d992cad66480cebb5f022358809d9669c1172f26152cae38";
+    byte[] example = "{\"id\":\"08bc6cd2-7da5-4708-8e61-6754ee0c158c\",\"created_on\":1525703268,\"event\":\"showschedule.deleted\",\"payload\":{\"from\":\"00:10:00\",\"until\":\"06:00:00\",\"offset\":600,\"limit\":21000,\"rules\":{\"id\":\"34c1ec2e-0f62-4f9f-8585-b7c09b040014\",\"created_on\":\"2018-05-07T14:27:42+00:00\",\"changed_on\":null,\"label\":\"ShowSchedule\",\"offset\":600,\"limit\":21000,\"valid_from\":\"2018-05-10\",\"valid_until\":\"2018-05-10\",\"exception\":true,\"days\":[\"1\",\"2\",\"3\",\"4\",\"5\",\"7\"]}}}".getBytes();
+    String signature = "24388cb0112ac5448ba4da3079260c3d28fdbb2a7ea59f1e791d7c09843c0ad8";
 
     SignatureValidatorInterceptor impl = new SignatureValidatorInterceptor();
 
     {
         SignatureValidatorInterceptor.put("RAD5", UUID.fromString("62d77be4-d41d-4878-bffc-ed4a047a9101"));
-        SignatureValidatorInterceptor.put("RAD3", UUID.fromString("ea4ae64f-a80e-4325-9d6e-f21322961ae8"));
     }
 
     @Test
@@ -35,7 +34,7 @@ public class SignatureValidatorInterceptorTest {
 
     @Test
     public void validate() throws NoSuchAlgorithmException, InvalidKeyException {
-        impl.validate(signature, example, "RAD3");
+        impl.validate(signature, example, "RAD5");
 
     }
 
