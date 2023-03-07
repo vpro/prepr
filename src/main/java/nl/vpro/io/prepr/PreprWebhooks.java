0d8a1ba2598235171433a0eea05aea8574ccae79
Michiel Meeuwissen <git@meeuw.org>
2023-03-07 13:07:22 +0100

    afaik this code hasn't worked for months, just will not try any longer.

--- src/main/java/nl/vpro/io/prepr/PreprWebhooks.java
+++ src/main/java/nl/vpro/io/prepr/PreprWebhooks.java
@@ -19,6 +19,12 @@ public interface PreprWebhooks {
 
     PreprWebhook create(String url, String... events);
 
+    /**
+     *  {"code":405,"message":"The PUT method is not supported for this route. Supported methods: GET, HEAD, DELETE.","dbtrace":"PRP.1678189824.9643"}
+     *
+     * @deprecated Seems to be broken. Just delete and recreate?
+     */
+    @Deprecated
     PreprWebhook put(PreprWebhook wh);
 
     void delete(UUID webhook);
